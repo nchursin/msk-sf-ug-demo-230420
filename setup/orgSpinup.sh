0c0dce73e465e822cae421887443fe63111fd458
Nikita Chursin <chursinn91@gmail.com>
2020-04-23 12:22:14 +0300

    single script to spin up org

--- setup/orgSpinup.sh
+++ setup/orgSpinup.sh
@@ -16,10 +16,15 @@ if [ ! -z $DEVHUB ]; then
     DEVHUB_FLAG="-v $DEVHUB"
 fi
 
-if [ ! -z $ALIAS ]; then
-    ALIAS_FLAG="-a $ALIAS"
+if [ -z $ALIAS ]; then
+    echo "Alias not specified! Aborting"
+    exit 1;
 fi
+
+ALIAS_FLAG="-a $ALIAS"
 
 echo "Creating org..."
 echo "sf force:org:create $DEFAULT_FLAG $DEVHUB_FLAG $ALIAS_FLAG -f config/project-scratch-def.json"
 sfdx force:org:create $DEFAULT_FLAG $DEVHUB_FLAG $ALIAS_FLAG -f config/project-scratch-def.json
+
+./setup/orgSetup.sh
